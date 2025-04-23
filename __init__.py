40881a877e80f8d384faadaaf7323ee73cffde85
Yuan-Man <68322456+Yuan-ManX@users.noreply.github.com>
2025-04-23 15:49:39 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,19 @@
+from .nodes import LoadDiaModel, InputDiaText, LoadDiaAudio, DiaTTS, SaveDiaAudio
 
+NODE_CLASS_MAPPINGS = {
+    "LoadDiaModel": LoadDiaModel,
+    "InputDiaText": InputDiaText,
+    "LoadDiaAudio": LoadDiaAudio,
+    "DiaTTS": DiaTTS,
+    "SaveDiaAudio": SaveDiaAudio,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "LoadDiaModel": "Load Dia Model",
+    "InputDiaText": "Input Dia Text",
+    "LoadDiaAudio": "Load Dia Audio",
+    "DiaTTS": "Dia TTS",
+    "SaveDiaAudio": "Save Dia Audio",
+} 
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
